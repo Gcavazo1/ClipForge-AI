540fdea16db012828e5419c098d3af71f08a877c
GigaCode <gcavazo1@gmail.com>
2025-06-28 21:47:37 -0500

    Updated App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import App from './App';
 import { ToastProvider } from './components/ui/toast';
 import { performanceMonitor } from './lib/performance/performance-monitor';
 import './index.css';
-import './styles/magicMouse.css';
 
 // Start performance monitoring
 performanceMonitor.startTimer('app-initialization');
